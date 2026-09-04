00285fdbc319bbb1fa19c0842106a9ce79828113
wiamabd <wiam@DESKTOP-PK61L7K.localdomain>
2026-09-04 20:25:49 +0100

    parse additions

--- src/parsing.c
+++ src/parsing.c
@@ -1,5 +1,6 @@
 #include "../codexion.h"
 
+#include <stdio.h>
 static int	is_positive_number(char *str)
 {
 	int	i;
@@ -7,12 +8,14 @@ static int	is_positive_number(char *str)
 	if (!str || !str[0])
 		return (0);
 	i = 0;
-	while (str[i])
-	{
-		if (str[i] < '0' || str[i] > '9')
-			return (0);
+	while ((9 <= str[i] && str[i] <= 13) || (str[i] == 32))
 		i++;
-	}
+	while ((str[i] >= '0' && str[i] <= '9'))
+		i++;
+	while ((9 <= str[i] && str[i] <= 13) || (str[i] == 32))
+		i++;
+	if(str[i] != '\0')
+		return(0);
 	return (1);
 }
 
@@ -25,14 +28,22 @@ static int	ascii_to_long(char *str, long *result)
 		return (0);
 	number = 0;
 	i = 0;
-	while (str[i])
+	while ((9 <= str[i] && str[i] <= 13) || (str[i] == 32))
+		i++;
+	while ((!((9 <= str[i] && str[i] <= 13) || (str[i] == 32)) )&&( str[i]) )
 	{
+
 		if (number > (LONG_MAX - (str[i] - '0')) / 10)
-			return (0);
+		{	printf("gggg %c\n",  str[i]);
+			return (0);}
 		number = number * 10 + (str[i] - '0');
 		i++;
 	}
+	
+	if (number>INT_MAX)
+		return(0);
 	*result = number;
+	
 	return (1);
 }
 
@@ -42,6 +53,7 @@ static int	parse_numbers(char **argv, t_config *config)
 		return (0);
 	if (!ascii_to_long(argv[2], &config->time_to_burnout))
 		return (0);
+		
 	if (!ascii_to_long(argv[3], &config->time_to_compile))
 		return (0);
 	if (!ascii_to_long(argv[4], &config->time_to_debug))
